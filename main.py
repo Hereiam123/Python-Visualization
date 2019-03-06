32e51565088d1ed9350867f9de61327c02e8f7d7
Brian De Maio <bdemaio1@gmail.com>
2019-03-06 10:47:19 -0500

    Added some styling and legends

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from bokeh.plotting import figure, output_file, show, ColumnDataSource, save
 from bokeh.models.tools import HoverTool
+from bokeh.transform import factor_cmap
+from bokeh.palettes import Blues8
 import pandas
 
 # Read in csv
@@ -20,8 +22,13 @@ p = figure(y_range=car_list, plot_width=800, plot_height=600, title="Cars with T
 
 # add a line renderer with legend and line thickness
 p.hbar(
-    y="Car", right="Horsepower", left=0, height=0.4, color='orange', fill_alpha=0.5, source=source
+    y="Car", right="Horsepower", left=0, height=0.4, fill_color=factor_cmap('Car', palette=Blues8, factors=car_list), fill_alpha=0.5, source=source, legend='Car'
 )
+
+# Add Legend
+p.legend.orientation = 'vertical'
+p.legend.location='top_right'
+p.legend.label_text_font_size='10px'
 
 # Add Tooltips
 hover = HoverTool()
