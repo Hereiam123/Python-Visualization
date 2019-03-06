9974a12077d0cac80d3a1b15f1e6af08cc1dae65
Brian De Maio <brian_demaio@wgresorts.com>
2019-03-06 09:28:00 -0500

    Visualizing car data

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from bokeh.plotting import figure, output_file, show
+import pandas
 
-# prepare some data
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
+# Read in csv
+df = pandas.read_csv('cars.csv')
+
+car = df['Car']
+hp = df['Horsepower']
 
 # output to static HTML file
 output_file("lines.html", title="line plot example")
 
 # create a new plot with a title and axis labels
-p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+p = figure(y_range=car, plot_width=800, plot_height=600, title="Cars with Top Horsepower",
+           x_axis_label='Horsepower', tools="")
 
 # add a line renderer with legend and line thickness
-p.line(x, y, legend="Temp.", line_width=2)
+p.hbar(
+    y=car, right=hp, left=0, height=0.4, color='orange', fill_alpha=0.5
+)
 
 # show the results
 show(p)
